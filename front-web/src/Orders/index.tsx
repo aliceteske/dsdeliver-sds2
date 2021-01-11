843750681f0ca3109ed4ead55657da7a1b080206
alice.teske <aliceteske@hotmail.com>
2021-01-11 13:52:38 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,30 @@
 import './styles.css';
-import React from 'react';
+import React, { useState } from 'react';
 import StepsHeaders from './StepsHeader';
+import OrderLocation from "./OrderLocation";
 import './styles.css';
+import ProductsList from './ProductsList';
+import { useEffect } from 'react';
+import { Product, OrderLocationData } from './types';
+import { fetchProducts } from '../api';
+
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
+
   return (
     <div className="orders-container">
       <StepsHeaders />
+      <ProductsList products={products} />
+      <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
     </div>
   );
 }
